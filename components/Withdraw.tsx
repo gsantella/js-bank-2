507a6e6203f6c0601df0989b1fa4aa8fa46f2f5b
btbare <btbare2002@gmail.com>
2026-03-31 20:16:35 -0400

    replaced Mock.io with Firestore as database

--- components/Withdraw.tsx
+++ components/Withdraw.tsx
@@ -2,45 +2,75 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-
+import { useTransaction } from "@/app/context/TransactionContext";
+import { db } from "../app/firebase";
+import { collection, getDocs, Transaction } from "firebase/firestore"
 export default function Withdraw() {
-  // State to store all accounts fetched from API
-  const [accounts, setAccounts] = useState<any[]>([]);
+
+  // State to store all account fetched from API
+  const [account, setAccount] = useState<any[]>([]);
+
   // State to store selected account ID
-  const [accountId, setAccountId] = useState("");
+  const [accountID, setAccountID] = useState("");
+
   // State to store withdrawal amount
   const [amount, setAmount] = useState("");
+
   // Next.js router for navigation
   const router = useRouter();
-  // Fetch accounts when component loads
+
+  // storing context
+  const { setTransaction } = useTransaction()
+
+  
+  // Fetch account data when component mounts
   useEffect(() => {
-    fetch("https://695f03af7f037703a8128fbf.mockapi.io/api/v1/Account")
-      .then((res) => res.json()) // Convert response to JSON
-      .then((data) => setAccounts(data)); // Store accounts in state
-  }, []); // Empty dependency array = runs once on mount
+    const fetchAccount = async (): Promise<void> => {
+      try{
+        const querySnapshot = await getDocs(collection(db, "Accounts"));
+        const accountList = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setAccount(accountList)
+      } catch (err){
+        console.log(err)
+      }
+    }
+    fetchAccount();
+  })
+
+//
+//
+//
+//
 
   // Function to handle withdrawal action
   const handleWithdraw = () => {
     // Validate input: must have account selected and amount > 0
-    if (!accountId || Number(amount) <= 0) {
+    if (!accountID || Number(amount) <= 0) {
       alert("Enter valid details");
       return;
     }
+
+    setTransaction({ account, accountID, amount, type: "withdraw"})
+
     // Redirect to pending page with query params
     router.push(
-      `/pending?accountId=${accountId}&amount=${amount}&type=withdraw`
+      `/pending`
     );
   };
+
   return (
     <div style={{ padding: 20 }}>
       <h2>Withdraw Funds</h2>
 
       {/* Dropdown to select account */}
-      <select value={accountId} onChange={(e) => setAccountId(e.target.value)}>
+      <select value={accountID} onChange={(e) => setAccountID(e.target.value)}>
         <option value="">Select Account</option>
 
-        {/* Loop through accounts and create options */}
-        {accounts.map((acc) => (
+        {/* Loop through account and create options */}
+        {account.map((acc) => (
           <option key={acc.id} value={acc.id}>
             {acc.id} (${acc.balance})
           </option>
